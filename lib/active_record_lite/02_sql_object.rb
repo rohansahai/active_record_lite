b888e64be7f0386f13e227b783affdc26681dea7
Rohan Sahai <rsahai91@gmail.com>
2014-04-18 14:53:29 -0700

    Getter, Setters fixed, working on insert

--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -7,7 +7,13 @@ class SQLObject < AttrAccessorObject
     if @column_names.nil?
       @column_names = DBConnection.execute2("SELECT * FROM #{self.table_name}")
       @column_names[0].each do |column_name|
-        my_attr_accessor(column_name)
+        define_method("#{column_name}") {
+          @attributes[column_name.to_sym]
+        }
+      
+        define_method("#{column_name}=") do |arg|
+          @attributes[column_name.to_sym] = arg
+        end
       end
       @column_names = @column_names[0]
     else
@@ -32,6 +38,10 @@ class SQLObject < AttrAccessorObject
     SELECT *
     FROM #{self.table_name}
     SQL
+    self.parse_all(hash_objects)
+  end
+  
+  def self.parse_all(hash_objects)
     ruby_objects = []
     hash_objects.each do |hash|
       ruby_objects << self.new(hash)
@@ -40,7 +50,12 @@ class SQLObject < AttrAccessorObject
   end
 
   def self.find(id)
-    # ...
+    object = DBConnection.execute(<<-SQL)
+    SELECT *
+    FROM #{self.table_name}
+    WHERE id = #{id}
+    SQL
+    p self.new(object[0])
   end
 
   def attributes
@@ -48,7 +63,15 @@ class SQLObject < AttrAccessorObject
   end
 
   def insert
-    # ...
+    col_names = @attributes.keys.join(", ")
+    value_array = attribute_values
+    question_marks = (["?"] * value_array.length).join(", ")
+    DBConnection.execute(<<-SQL, value_array)
+    INSERT INTO
+    #{self.table_name} (#{col_names})
+    VALUES
+    #{question_marks}
+    SQL
   end
 
   def initialize(params = {})
@@ -70,11 +93,17 @@ class SQLObject < AttrAccessorObject
   end
 
   def attribute_values
-    # ...
+    values = []
+    @attributes.each do |key, value|
+      next if key == :id
+      values << value
+    end
+    values
   end
 end
 
 class Cat < SQLObject
 end
 
-Cat.all
+breakfast = Cat.find(1)
+p breakfast.insert
